4d667fb730196d20f7cf9248ac3fc6acabb27e08
DennyChiu <100756589+Dennychiou@users.noreply.github.com>
2026-03-03 00:40:04 +0800

    Update TriggerDetection.cs

--- passthrough mode testing/Assets/Scripts/TriggerDetection.cs
+++ passthrough mode testing/Assets/Scripts/TriggerDetection.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class TriggerDetection : MonoBehaviour
 {
-    public GameObject RawImage; // 指向你的 RawImage 物件
+    public GameObject RawImage; 
 
     void Start()
     {
-        RawImage.SetActive(false); // 初始時隱藏 RawImage
+        RawImage.SetActive(false); 
     }
 
     void OnTriggerEnter(Collider other)
     {
-        // 檢查碰撞物是否為車輛
         if (other.CompareTag("Car"))
         {
             Debug.Log("DetectionObject has triggered with the car!");
-            RawImage.SetActive(true); // 顯示 RawImage
-            Debug.Log("RawImage active state: " + RawImage.activeSelf); // 输出 RawImage 的活动状态
+            RawImage.SetActive(true);
+            Debug.Log("RawImage active state: " + RawImage.activeSelf); 
         }
     }
+
 }
