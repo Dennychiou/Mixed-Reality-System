9ec6e3a425df7a27ee54fd91d51e08cc4d4fc128
DennyChiu <100756589+Dennychiou@users.noreply.github.com>
2026-03-03 01:21:51 +0800

    Update CarController.cs

--- passthrough mode testing/Assets/Scripts/car automove scripts/CarController.cs
+++ passthrough mode testing/Assets/Scripts/car automove scripts/CarController.cs
@@ -2,29 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
-{
+public class CarController : MonoBehaviour{
     public float speed = 5f;
-
     private bool isStopped = false;
-
-    void Update()
-    {
-        // 检查红绿灯状态
-        if (R1.GetCurrentState() == R1.TrafficLightState.Red)
-        {
-            // 停车
+    void Update(){
+        if (R1.GetCurrentState() == R1.TrafficLightState.Red){
             isStopped = true;
         }
-        else if (R1.GetCurrentState() == R1.TrafficLightState.Green)
-        {
-            // 行驶
+        else if (R1.GetCurrentState() == R1.TrafficLightState.Green){
             isStopped = false;
         }
-
-        if (!isStopped)
-        {
-            // 车辆行驶逻辑
+        if (!isStopped){
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
     }
